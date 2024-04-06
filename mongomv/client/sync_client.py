4c8b639c28c88431e110f6777ba6b47e0859dca1
averagepythonfan <djonvorobei@bk.ru>
2024-04-06 22:06:55 +0300

    full working sync client

--- mongomv/client/sync_client.py
+++ mongomv/client/sync_client.py
@@ -1,7 +1,7 @@
-from typing import Any, Optional, List, Union
+from typing import Any, Literal, Optional, List, Union
 
 # from mongomv.services import PymongoService
-from mongomv.schemas import ModelEntity, ExperimentEntity, ModelParams
+from mongomv.schemas import ModelEntity, ExperimentEntity, ModelParams, Collections, FindBy
 from mongomv.services import PymongoService
 
 
@@ -45,6 +45,15 @@ class MongoMVClient:
                      tags: List[str],
                      params: Optional[List[ModelParams]] = [],
                      description: str = None) -> ModelEntity:
+        """Create an model instance.
+        
+        Requires name and tags. Optional params and description.
+        Return `ModelEntity` (mongomv.schemas.ModelEntity) instance.
+        Example:
+        >>> md = client.create_model(name="keras_model", tags=["dev", "v0.1"])
+        >>> md.id
+        ... ObjectId('66105f81426f1640c3d7e167')
+        """
         model = ModelEntity(
             name=name,
             tags=tags,
@@ -57,17 +66,54 @@ class MongoMVClient:
 
 
     def list_of_experiments(self, num: int = 10, page: int = 0):
+        """Return list of `ExperimentEntity` instances.
+        
+        May set numbers and page.
+        """
         cur = self.mongo_service.client.mongomv.experiments.find({})
-        return list(cur)[num*page:num*(page+1)]
+        lst = [ExperimentEntity(**el, service=self.mongo_service) for el in cur]
+        return lst[num*page:num*(page+1)]
 
 
     def list_of_models(self, num: int = 10, page: int = 0):
+        """Return list of `ModelEntity` instances.
+        
+        May set numbers and page.
+        """
         cur = self.mongo_service.client.mongomv.models.find({})
-        return list(cur)[num*page:num*(page+1)]
+        lst = [ModelEntity(**el, service=self.mongo_service) for el in cur]
+        return lst[num*page:num*(page+1)]
 
 
-    def find_experiment_by(self):
-        pass
+    def find_experiment_by(self,
+                           find_by: Literal["id", "name", "date", "tags"],
+                           value: str,
+                           is_list: bool = False):
+        """Find experiment by `id` or `name` or less than `date` or `tags`.
 
-    def find_model_by(self):
-        pass
+        May return list of experiments is `is_list` is `True`.
+        """
+        find_result = self.mongo_service.read(
+            collection=Collections.experiments,
+            find_by=FindBy(find_by),
+            value=value,
+            is_list=is_list
+        )
+        return find_result if is_list else find_result[0]
+
+
+    def find_model_by(self,
+                      find_by: Literal["id", "name", "date", "tags"],
+                      value: str,
+                      is_list: bool = False):
+        """Find model by `id` or `name` or less than `date` or `tags`.
+
+        May return list of experiments is `is_list` is `True`.
+        """
+        find_result = self.mongo_service.read(
+            collection=Collections.models,
+            find_by=FindBy(find_by),
+            value=value,
+            is_list=is_list
+        )
+        return find_result if is_list else find_result[0]
